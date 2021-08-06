68ee32b3b96144138a97b14930ee963c6de57c1b
Kolen Cheung <christian.kolen@gmail.com>
2021-08-06 14:51:41 -0700

    initial proof of concept

--- extension.js
+++ extension.js
@@ -3,7 +3,22 @@
 function activate(context) {
     return {
         extendMarkdownIt(md) {
-            return md.use(require('markdown-it-emoji'));
+            // https://github.com/markdown-it/markdown-it-container/issues/23#issuecomment-389160929
+            return md.use(
+                require('markdown-it-container'),
+                'dynamic',{
+                    validate: function() { return true; },
+                    render: function(tokens, idx) {
+                        var token = tokens[idx];
+                
+                        if (token.nesting === 1) {
+                            return '<div class="' + token.info.trim() + '">';
+                        } else {
+                            return '</div>';
+                        }
+                    },
+                }
+            );
         }
     };
 }
